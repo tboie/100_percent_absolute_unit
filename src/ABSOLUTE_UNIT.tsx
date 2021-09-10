fc40afa32ea6dc9fdb8912ee40ed57024bd65e74
Timothy Boie <admin@Timothys-MBP.home>
2021-09-09 20:22:11 -0400

    grace

--- src/ABSOLUTE_UNIT.tsx
+++ src/ABSOLUTE_UNIT.tsx
@@ -1,4 +1,5 @@
 import STYLE from "./ABSOLUTE_UNIT.module.scss";
+import { useState } from "react";
 
 export type TYPE_ABSOLUTE_UNIT = {
   i: string;
@@ -10,15 +11,17 @@ export type TYPE_ABSOLUTE_UNIT = {
   sW?: number;
   sH?: number;
   c?: string[];
+  e?: boolean;
   m?: TYPE_ABSOLUTE_UNIT[];
 };
 
 const ABSOLUTE_UNIT = (p: TYPE_ABSOLUTE_UNIT) => {
+  const [e, sE] = useState(!!p.e);
   return (
     <div
       className={STYLE.ABSOLUTE_UNIT}
       style={{
-        transform: `translate(${p.x * 2}%,${p.y * 2}%)`,
+        transform: `translate(${p.x * 2}%, ${p.y * 2}%)`,
         width: `${p.w}%`,
         height: `${p.h}%`,
       }}
