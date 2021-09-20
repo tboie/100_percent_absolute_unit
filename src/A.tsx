67691e792e85e224f65b1d6343291fb92d5ac24c
Timothy Boie <admin@Timothys-MacBook-Pro.local>
2021-09-20 16:33:17 -0400

    coordination

--- src/A.tsx
+++ src/A.tsx
@@ -5,7 +5,7 @@ const A = (p: T) => (
   <div
     className={S.A}
     style={{
-      transform: `translate(${p.x * 2}%,${p.y * 2}%)`,
+      transform: `translate(${(p.x * 100) / p.w}%,${(p.y * 100) / p.h}%)`,
       width: `${p.w}%`,
       height: `${p.h}%`,
       zIndex: p.z,
